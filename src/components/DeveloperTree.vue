4314d532dfa4a24c67487b77d5d5adb599642fe7
Kulikov Egor <benstiller68@gmail.com>
2019-10-02 23:23:36 +0300

    prepare 3 lvl children

--- src/components/DeveloperTree.vue
+++ src/components/DeveloperTree.vue
@@ -16,7 +16,7 @@
           <span class="font-italic" v-text="item.description"></span>
         </template>
         <template v-if="item.image" v-slot:icon>
-          <v-avatar >
+          <v-avatar>
             <img :src="serverUrl + item.image" />
           </v-avatar>
         </template>
@@ -28,9 +28,11 @@
                 <v-list-item-content class="align-self-start">
                   <v-card-title v-text="child.name" />
                   <v-divider v-if="child.description" class="mx-2" />
-                  <v-card-text v-if="child.description"
+                  <v-card-text
+                    v-if="child.description"
                     class="font-italic"
-                    v-text="child.description" />
+                    v-text="child.description"
+                  />
                 </v-list-item-content>
                 <v-list-item-avatar v-if="child.image" tile width="auto">
                   <img :src="serverUrl + child.image" />
@@ -42,9 +44,11 @@
               <v-list-item-content class="align-self-start">
                 <v-card-title v-text="child.name" />
                 <v-divider v-if="child.description" class="mx-2" />
-                <v-card-text v-if="child.description"
+                <v-card-text
+                  v-if="child.description"
                   class="font-italic"
-                  v-text="child.description" />
+                  v-text="child.description"
+                />
               </v-list-item-content>
               <v-list-item-avatar v-if="child.image" tile width="auto">
                 <img :src="serverUrl + child.image" />
@@ -58,86 +62,93 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
 export default {
-  name: 'DeveloperTree',
+  name: "DeveloperTree",
   props: {
     items: {
-      type: Array,
-    },
+      type: Array
+    }
   },
   computed: {
-    ...mapState('tree', ['serverUrl']),
+    ...mapState("tree", ["serverUrl"])
   },
   methods: {
     calculateLine(a, b) {
       return a - b;
-    },
+    }
   },
   mounted() {
     const halfScreen = screen.width / 2;
-    const radToDeg = (rad) => (rad * 180) / Math.PI;
+    const radToDeg = rad => (rad * 180) / Math.PI;
     const hypotenus = (a, b) => Math.sqrt(a ** 2 + b ** 2);
-    const calculateLine = (a, b) => a - b;
 
-    Object.values(this.$refs).forEach((item) => {
+    Object.values(this.$refs).forEach(item => {
       const coordsParent = item[0].$el.getBoundingClientRect();
       const centerParent = {
         x: coordsParent.left + coordsParent.width / 2,
-        y: coordsParent.top + coordsParent.height / 2,
+        y: coordsParent.top + coordsParent.height / 2
       };
 
-      Object.values(item[0].$children).forEach((child) => {
-        if (child.$el.classList[0] === 'v-card') {
+      Object.values(item[0].$children).forEach(child => {
+        if (child.$el.classList[0] === "v-card") {
           const coordsChild = child.$el.getBoundingClientRect();
           const isLeftChild = coordsChild.right > centerParent.x;
           const start = {
             x: centerParent.x,
-            y: centerParent.y - 12,
+            y: centerParent.y - 12
           };
           const end = {
             x: isLeftChild ? coordsChild.left : coordsChild.right,
-            y: coordsChild.top + coordsChild.height / 2,
+            y: coordsChild.top + coordsChild.height / 2
           };
-          const lineA = start.x - end.x;
-          const lineB = start.y - end.y;
-          const lineC = hypotenus(calculateLine(start.x, end.x), calculateLine(start.y, end.y));
 
-          Object.values(child.$el.children).forEach((arrow) => {
-            if (arrow.className === 'arrow') {
+          Object.values(child.$el.children).forEach(arrow => {
+            if (arrow.className === "arrow") {
+              const lineA = start.x - end.x;
+              const lineB = start.y - end.y;
+              const lineC = hypotenus(start.x - end.x, start.y - end.y);
               const incline = Math.asin(lineB / lineC);
               const degIncline = radToDeg(incline);
               const correctRotate = isLeftChild ? 0 : 180;
               const correctPos = isLeftChild ? -20 : 220;
 
-              arrow.style.transform = `rotate(${correctRotate - degIncline}deg)`;
-              arrow.style.position = 'absolute';
+              arrow.style.transform = `rotate(${correctRotate -
+                degIncline}deg)`;
+              arrow.style.position = "absolute";
               arrow.style.left = `${lineA + correctPos}px`;
               arrow.style.top = `${lineB + 45}px`;
               arrow.style.width = `${lineC}px`;
             }
           });
-          Object.values(child.$children).forEach((babyChild) => {
-            if (babyChild.$el.classList[0] === 'baby-child') {
+          Object.values(child.$children).forEach(babyChild => {
+            if (babyChild.$el.classList[0] === "baby-child") {
               const babyChildElem = babyChild.$el;
               const coordsBabyChildElem = babyChildElem.getBoundingClientRect();
               if (coordsBabyChildElem.x > halfScreen) {
-                babyChildElem.style.left = '18vw';
-
+                babyChildElem.style.left = "18vw";
                 const startBabyChild = {
-                  x: babyChildElem.left,
-                  y: babyChildElem.top + babyChildElem.height / 2,
+                  x: coordsBabyChildElem.x,
+                  y: coordsBabyChildElem.top + coordsBabyChildElem.height / 2
                 };
                 const endBabyChild = {
                   x: coordsChild.right,
-                  y: coordsChild.top + coordsChild.height / 2,
+                  y: coordsChild.top + coordsChild.height / 2
                 };
-                const lineA = start.x - end.x;
-                const lineB = start.y - end.y;
-                const lineC = this.hypotenus(lineA, lineB);
+                const lineA = startBabyChild.x - endBabyChild.x;
+                const lineB = startBabyChild.y - endBabyChild.y;
+                const lineC = hypotenus(lineA, lineB);
+                const incline = Math.asin(lineB / lineC);
+                const degIncline = radToDeg(incline);
+                console.log(degIncline);
+                babyChildElem.style.transform = `rotate(${degIncline}deg)`;
+                babyChildElem.style.position = "absolute";
+                babyChildElem.style.left = `${lineA}px`;
+                babyChildElem.style.top = `${lineB}px`;
+                babyChildElem.style.width = `${lineC}px`;
               } else {
-                babyChildElem.style.right = '18vw';
+                babyChildElem.style.right = "18vw";
               }
             }
           });
@@ -146,7 +157,7 @@ export default {
     });
 
     console.log(this.$refs);
-  },
+  }
 };
 </script>
 
@@ -194,7 +205,7 @@ export default {
   .v-card {
     max-width: calc(15vw);
   }
-  .v-card .v-sheet .theme--light{
+  .v-card .v-sheet .theme--light {
     margin-right: 0px;
   }
   .v-card__title {
@@ -221,10 +232,10 @@ export default {
   .v-timeline:not(.v-timeline--dense):not(.v-timeline--reverse)
     .v-timeline-item--before
     .v-timeline-item__body {
-      max-width: calc(50% - 329px);
+    max-width: calc(50% - 329px);
   }
 
-  .v-timeline-item:nth-child(even){
+  .v-timeline-item:nth-child(even) {
     padding-left: 30%;
   }
 }
@@ -233,23 +244,23 @@ export default {
   background: linear-gradient(to left, #333, #333, #fff, #fff);
 }
 .arrow:before,
-  .arrow:after {
-    content: "";
-    display: block;
-    position: absolute;
-    width: 0;
-    height: 0;
-    border: 9px solid transparent;
-    border-right: 0;
-    top: -7.5px;
-    right: -1px;
-    border-left-color: black;
-    }
+.arrow:after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 0;
+  height: 0;
+  border: 9px solid transparent;
+  border-right: 0;
+  top: -7.5px;
+  right: -1px;
+  border-left-color: black;
+}
 .v-card.v-sheet.theme--light.favorite {
   background: #4700f387;
   position: relative;
 }
-.baby-child{
+.baby-child {
   position: absolute;
 }
 </style>
